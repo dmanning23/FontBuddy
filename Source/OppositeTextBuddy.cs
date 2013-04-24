4a5986e54b68b46166e3e44c3ac6c76ceb0500e2
unknown <dmanning23@hotmail.com>
2013-04-23 19:23:00 -0500

    fixed opposites text

--- Source/OppositeTextBuddy.cs
+++ Source/OppositeTextBuddy.cs
@@ -50,7 +50,7 @@ namespace FontBuddyLib
 		/// <param name="dTime">the current game time in seconds</param>
 		public override float Write(string strText, Vector2 Position, Justify eJustification, float fScale, Color myColor, SpriteBatch mySpriteBatch, double dTime)
 		{
-			float fKerning = (Font.MeasureString(" ").X * 0.25f) * fScale;
+			float fKerning = Font.Spacing * fScale;
 
 			//Get the correct location
 			Vector2 textSize = Font.MeasureString(strText) * fScale;
