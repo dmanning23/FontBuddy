c5e9f6d214239b1ec36cf48a1497e697fe42d397
unknown <dmanning23@hotmail.com>
2014-02-09 02:00:55 -0600

    added convenience method

--- Source/FontBuddy.cs
+++ Source/FontBuddy.cs
@@ -64,10 +64,36 @@ namespace FontBuddyLib
 		/// <param name="myColor">the color to draw the text</param>
 		/// <param name="mySpriteBatch">spritebatch to use to render the text</param>
 		/// <param name="dTime">Most of the other font buddy classes use time somehow, but can jsut send in 0.0f for this dude or ignoer it</param>
-		public virtual float Write(string strText, Vector2 Position, Justify eJustification, float fScale, Color myColor,
-		                           SpriteBatch mySpriteBatch, double dTime = 0.0f)
+		public virtual float Write(string strText, 
+			Vector2 Position, 
+			Justify eJustification, 
+			float fScale, 
+			Color myColor,
+			SpriteBatch mySpriteBatch, 
+			double dTime = 0.0f)
 		{
 			return DrawText(strText, Position, eJustification, fScale, myColor, mySpriteBatch, dTime);
+		}
+
+		/// <summary>
+		/// write something on the screen
+		/// </summary>
+		/// <param name="strText">the text to write on the screen</param>
+		/// <param name="Position">where to write at... either upper left, upper center, or upper right, depending on justication</param>
+		/// <param name="eJustification">how to justify the text</param>
+		/// <param name="fScale">how big to write.  This is not a point size to draw at, it is a multiple of the default font size!</param>
+		/// <param name="myColor">the color to draw the text</param>
+		/// <param name="mySpriteBatch">spritebatch to use to render the text</param>
+		/// <param name="dTime">Most of the other font buddy classes use time somehow, but can jsut send in 0.0f for this dude or ignoer it</param>
+		public virtual float Write(string strText, 
+			Point Position, 
+			Justify eJustification, 
+			float fScale, 
+			Color myColor,
+			SpriteBatch mySpriteBatch, 
+			double dTime = 0.0f)
+		{
+			return DrawText(strText, new Vector2(Position.X, Position.Y), eJustification, fScale, myColor, mySpriteBatch, dTime);
 		}
 
 		/// <summary>
