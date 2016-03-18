973d3590fa5b1bc3794ee74f35427d811a12949e
Dan Manning <dmanning23@hotmail.com>
2016-03-17 23:31:44 -0500

    fontbuddy in a nuget

--- FontBuddy/FontBuddy/Properties/AssemblyInfo.cs
+++ FontBuddy/FontBuddy/Properties/AssemblyInfo.cs
@@ -5,24 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FontBuddy.Windows.XNA")]
-[assembly: AssemblyProduct("FontBuddy.Windows.XNA")]
+[assembly: AssemblyTitle("FontBuddy")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2013")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FontBuddy")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("5b9ca932-052e-40f0-9fde-83886c0522d5")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("19082761-4b90-41f5-b62d-55a2f95d5833")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
