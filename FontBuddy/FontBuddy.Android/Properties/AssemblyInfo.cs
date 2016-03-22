dc088cc6eaec338137097955d13aa2c115553574
Dan Manning <dmanning23@hotmail.com>
2016-03-21 21:35:50 -0500

    android nuget

--- FontBuddy/FontBuddy.Android/Properties/AssemblyInfo.cs
+++ FontBuddy/FontBuddy.Android/Properties/AssemblyInfo.cs
@@ -1,26 +1,20 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FontBuddy")]
+[assembly: AssemblyTitle("FontBuddy.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FontBuddy")]
+[assembly: AssemblyProduct("FontBuddy.Android")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("19082761-4b90-41f5-b62d-55a2f95d5833")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ FontBuddy/buildNugetPackages.ps1
@@ -0,0 +1,3 @@
+nuget pack .\FontBuddy.Android\FontBuddy.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+
+nuget pack .\FontBuddy.WindowsGL\FontBuddy.WindowsGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
