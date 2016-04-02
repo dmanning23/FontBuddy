0e89bf84653341d9c9e0a56250eab0329d2826ff
Dan Manning <dmanning23@hotmail.com>
2016-04-02 12:10:53 -0500

    redid windows nuget pacakges

--- FontBuddy/FontBuddy.DesktopGL/Properties/AssemblyInfo.cs
+++ FontBuddy/FontBuddy.DesktopGL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FontBuddy.WindowsGL")]
+[assembly: AssemblyTitle("FontBuddy.DesktopGL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FontBuddy.WindowsGL")]
+[assembly: AssemblyProduct("FontBuddy.DesktopGL")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- FontBuddy/buildNugetPackages.ps1
+++ FontBuddy/buildNugetPackages.ps1
@@ -1,3 +1,5 @@
 nuget pack .\FontBuddy.Android\FontBuddy.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 
-nuget pack .\FontBuddy.WindowsGL\FontBuddy.WindowsGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\FontBuddy.DesktopGL\FontBuddy.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+
+nuget pack .\FontBuddy.WindowsUniversal\FontBuddy.WindowsUniversal.nuspec -IncludeReferencedProjects -Prop Configuration=Release
