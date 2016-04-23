27fd171a13c37116d898f865fe2a8b87f70f0d9e
Dan Manning <dmanning23@hotmail.com>
2016-04-22 22:11:51 -0500

    updated nuget package

--- FontBuddy/buildNugetPackages.ps1
+++ FontBuddy/buildNugetPackages.ps1
@@ -1,7 +1,5 @@
 nuget pack .\FontBuddy.Android\FontBuddy.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-
 nuget pack .\FontBuddy.DesktopGL\FontBuddy.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-
 nuget pack .\FontBuddy.iOS\FontBuddy.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-
 nuget pack .\FontBuddy.WindowsUniversal\FontBuddy.WindowsUniversal.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget push *.nupkg
