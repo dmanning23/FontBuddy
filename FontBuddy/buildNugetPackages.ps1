3583dbf6eae9535aaa7190fce8e60c8b4a703146
Dan Manning <dmanning23@hotmail.com>
2016-04-23 13:27:04 -0500

    moved everything into a pcl

--- FontBuddy/buildNugetPackages.ps1
+++ FontBuddy/buildNugetPackages.ps1
@@ -1,5 +1,2 @@
-nuget pack .\FontBuddy.Android\FontBuddy.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\FontBuddy.DesktopGL\FontBuddy.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\FontBuddy.iOS\FontBuddy.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\FontBuddy.WindowsUniversal\FontBuddy.WindowsUniversal.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\FontBuddy.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 nuget push *.nupkg
