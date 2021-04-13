2ea24e637f3a98df382af4e730c5f31e459844cb
Dan Manning <dmanning@oceaneering.com>
2021-04-13 11:24:13 -0500

    change stroke with of outlined font

--- FontBuddy/FontBuddy.SharedProject/FontBuddyPlusStroked.cs
+++ FontBuddy/FontBuddy.SharedProject/FontBuddyPlusStroked.cs
@@ -5,9 +5,16 @@ namespace FontBuddyLib
 {
 	public class FontBuddyPlusStroked : FontBuddyPlus
 	{
+		public int OutlineSize { get; private set; }
+
+		public FontBuddyPlusStroked(int outlineSize = 1)
+		{
+			OutlineSize = outlineSize;
+		}
+
 		protected override FontSystem CreateFontSystem(GraphicsDevice device)
 		{
-			return FontSystemFactory.CreateStroked(device, 1);
+			return FontSystemFactory.CreateStroked(device, OutlineSize);
 		}
 	}
 }
